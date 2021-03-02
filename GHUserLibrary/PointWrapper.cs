dfecad7e9b64f0ca3ac7149b77ac0f3a3d53f1cb
Dmytro Radomtsev <35939248+dradomtsev@users.noreply.github.com>
2021-03-02 20:36:26 +0200

    Add project files.

---
+++ GHUserLibrary/PointWrapper.cs
@@ -0,0 +1,20 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//using Rhino;
+using Rhino.Geometry;
+
+namespace GHUserLibrary
+{
+    public class PointWrapper
+    {
+        public Point3d pPointInt;
+        public PointWrapper(Point3d pPointExt)
+        {
+            pPointInt = pPointExt;
+        }
+    }
+}
